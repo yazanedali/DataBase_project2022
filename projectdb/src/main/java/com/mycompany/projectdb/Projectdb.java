535271a465d7aa4a27e50039f26304b634f3f308
yazanedali <edaliyazan6@gmail.com>
2024-07-10 14:29:24 +0300

    final project

---
+++ projectdb/src/main/java/com/mycompany/projectdb/Projectdb.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.projectdb;
+
+/**......
+ *
+ * @author user
+ */
+public class Projectdb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        new login_in().setVisible(true);
+    }
+}
